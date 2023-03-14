d7ca1de3c39afa791cb0149e2470a9b6352de17e
Noah Redford <noah.redford27@gmail.com>
2023-03-14 17:21:28 -0600

    added templates and set up mongodb

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,10 +1,18 @@
-
+require('../models/database')
+const Category = require('../models/Category')
 
 
 //GET / Homepage
 
 exports.homepage = async(req, res) => {
+    try {
 
-    res.render('index', { title: 'Redford Recipes - Home 🏠' });
-
+        const limitNumber = 5;
+        const categories = await Category.find({}).limit(limitNumber);
+        res.render('index', { title: 'Redford Recipes - Home 🏠', categories});
+    } catch (error){
+        res.status(500).send({message: error.message || "Error Occured"});
+    }
 }
+
+
